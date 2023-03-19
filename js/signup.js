5cfcd69c5b758d2ad79f6931e13b1868cd794daf
ebikdeli <ebikdeli@gmail.com>
2023-03-19 12:33:02 +0330

    signup updated

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,4 @@
-import {checkRulesBorder, disabledSubmitRule, signUpDataValidation} from './functions.js';
+import { checkRulesBorder, disabledSubmitRule, signUpDataValidation } from './functions.js';
 import { sendPostData } from './ajax.js';
 
 
@@ -11,22 +11,27 @@ submitButton.parentElement.addEventListener('click', e => {
     e.preventDefault();
     const ruleChecked = checkRulesBorder(acceptButton, submitButton);
     // * If rule checkbox is checked, validate form data
-    if(ruleChecked){
+    if (ruleChecked) {
         const email = document.querySelector('[name="email"]').value;
         const password = document.querySelector('[name="password"]').value;
         const passwordConfirm = document.querySelector('[name="password-confirm"]').value;
         // If data validation was successful, send data to server
-        if(signUpDataValidation(email, password, passwordConfirm)){
-            const url = 'http://127.0.0.1:8000/signup';
-            const data = {email: email, password: password};
-            const err = 'مشکلی در اتصال پیش آمده';
-            sendPostData(url, data, err)
-            .then(data => {
-                console.log(data);
-            })
-            .catch(err => {
-                console.log(err);
-            })
+        if (signUpDataValidation(email, password, passwordConfirm)) {
+            Swal.fire(
+                'ثبت نام موفق',
+                'ثبت نام با موفقیت انجام گرفت',
+                'success'
+            )
+            // const url = 'http://127.0.0.1:8000/signup';
+            // const data = {email: email, password: password};
+            // const err = 'مشکلی در اتصال پیش آمده';
+            // sendPostData(url, data, err)
+            // .then(data => {
+            //     console.log(data);
+            // })
+            // .catch(err => {
+            //     console.log(err);
+            // })
         }
     }
 })
